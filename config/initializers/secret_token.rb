fbb67cdcf18814a21bb7a013e502e66800e3cf14
Victor <cherokie@gmail.com>
2011-07-26 14:23:26 +0200

    pasamos a rails 3.0.9

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cherokie::Application.config.secret_token = '1d261fcb5759274c1bf48a1805a88dd92f8d8206f7d3e99855fb58725ae5a59b111b6e5bef8283440e42bbf43913950b31000bc8cf7a728bfd61d7fbd976f250'
---
+++ public/javascripts/twitter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+        $.getScript('http://twitter.com/statuses/user_timeline/cherokienet.json?callback=twitterCallback2&count=5');
+    });
+
+function twitterCallback2(obj) {
+    var twitters = obj;
+    var statusHTML = "";
+    var username = "";
+    var today = new Date();
+    for (var i=0; i<twitters.length; i++){
+        username = twitters[i].user.screen_name
+        statusHTML += ('<li><span>'+twitters[i].text+'</span> <a style="font-size:85%" href="http://twitter.com/'+username+'/statuses/'+twitters[i].id+'">'+today+'</a></li>')
+    }
+    $('#latest_tweet').html(statusHTML);
+}
